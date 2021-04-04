5520c57d0bd1388f4e671dae8c350770c9300cea
moshiulrabbi <mhrabby61@gmail.com>
2021-04-04 21:02:58 +0600

    url-/fixed

--- openbook/urls.py
+++ openbook/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('',views.home_view,name="home"),
-    path('login',views.login_view,name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("donatebook", views.donatebook_view, name="donatebook"),
-    path("viewbook", views.view_book, name="viewbook"),
-    path("register",views.register, name="register"),
+    path('login/',views.login_view,name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("donatebook/", views.donatebook_view, name="donatebook"),
+    path("viewbook/", views.view_book, name="viewbook"),
+    path("register/",views.register, name="register"),
     path('profile/',views.profile, name="profile"),
     path('viewbook/<int:book_id>', views.book_details,name="book_details"),
     path('lend/<int:book_id>', views.lend_view,name="lend"),
