a2f843cd0a63e8da75ebb9b71fcbefdffe5b1111
moshiulrabbi <mhrabby61@gmail.com>
2021-05-02 21:59:05 +0600

    base2-test

--- openbook/urls.py
+++ openbook/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     path('profile/',views.profile, name="profile"),
     path('viewbook/<int:book_id>', views.book_details,name="book_details"),
     path('lend/<int:book_id>', views.lend_view,name="lend"),
-    path('returnbook/<int:book_id>', views.return_book,name="returnbook")
+    path('returnbook/<int:book_id>', views.return_book,name="returnbook"),
+    path('base2',views.base2,name="base2")
 ]
 
 # url(r'^profile/(?P<username>\w+)/$',profile,name='profile'),
