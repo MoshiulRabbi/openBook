36758098b02d905b2621c1ca97a453df55164e27
moshiulrabbi <mhrabby61@gmail.com>
2021-07-05 20:57:31 +0600

    somechanges:wrench:

--- ecomApp/urls.py
+++ ecomApp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.productList,name="ecommerce"),
+    path('product/<int:pk>',views.orderItem,name="orderItem"),
+    path('addToCart/<int:pk>',views.addToCart,name="addToCart"),
+    path('cart/',views.cartList,name="cart"),
 ]
--- ecomApp/views.py
+++ ecomApp/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Product
+from .models import Cart, Product
+from django.urls import reverse
 
 # Create your views here.
 def productList(request):
     products = Product.objects.all()
     return render(request,"ecom/products.html",{"products":products})
+
+def orderItem(request,pk):
+    item = Product.objects.get(pk=pk)
+    return render(request,"ecom/orderItem.html",{"item":item})
+
+
+def cartList(request):
+    c = Cart.objects.all()
+    return render(request,"ecom/cart.html",{"c":c})
+
+
+def addToCart(request,pk):
+    product = Product.objects.get(pk=pk)
+    cartItem = Cart(orderedProduct = product)
+    cartItem.save()
+    return HttpResponseRedirect(reverse("cart"))
